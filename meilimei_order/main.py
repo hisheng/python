0fc36b0db6e735561df3ad9b81d8196ca11ccd20
张海生 <zhanghaisheng@qimao.com>
2021-07-06 15:57:50 +0800

    1

--- meilimei_order/main.py
+++ meilimei_order/main.py
@@ -1,19 +1,19 @@
 import csv
-columnName = ['day','all','login','sku','order','pay']
+
+columnName = ['day', 'all', 'login', 'sku', 'order', 'pay']
 
 users = [
-    [20171013,10000,1000,100,10,1],
-    [20171012,20000,2000,200,20,2],
-    [20171011,30000,3000,300,30,3]
+    [20171013, 10000, 1000, 100, 10, 1],
+    [20171012, 20000, 2000, 200, 20, 2],
+    [20171011, 30000, 3000, 300, 30, 3]
 ]
 
-f = open('order.csv','w')
+f = open('order.csv', 'w')
 csvf = csv.writer(f)
 
-print('file name is - '+ f.name)
+print('file name is - ' + f.name)
 
 csvf.writerow(columnName)
 csvf.writerows(users)
 
 f.close()
-
